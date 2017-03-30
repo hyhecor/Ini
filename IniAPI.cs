ff0f054826f3e9e4fb757a44f81cf656e454e79d
hhcho <hhcho@3ssoft.co.kr>
2017-03-30 17:49:36 +0900

    1

---
+++ IniAPI.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Ini
+{
+    public class IniAPI
+    {
+        const int __BUFF_SIZE = 32000;
+
+        //[DllImport("KERNEL32.DLL", EntryPoint = "GetPrivateProfileStringW", 
+        //    SetLastError = true, 
+        //    CharSet = CharSet.Unicode, 
+        //    ExactSpelling = true, 
+        //    CallingConvention = CallingConvention.StdCall)]
+        //private static extern int GetPrivateProfileString(string lpAppName, 
+        //    string lpKeyName, 
+        //    string lpDefault,
+        //    string lpReturnString, 
+        //    int nSize,
+        //    string lpFilename);
+
+        [DllImport("KERNEL32.DLL", EntryPoint = "GetPrivateProfileStringW", 
+            SetLastError = true, 
+            CharSet = CharSet.Unicode, 
+            ExactSpelling = true, 
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern int GetPrivateProfileString(
+            string section,
+            string key,
+            string def,
+            string retVal,
+            int size,
+            string filePath);
+
+        [DllImport("KERNEL32.DLL", EntryPoint = "WritePrivateProfileStringW", 
+            SetLastError = true, 
+            CharSet = CharSet.Unicode, 
+            ExactSpelling = true, 
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern long WritePrivateProfileString(
+            string section,
+            string key,
+            string val,
+            string filePath);
+
+
+        /// <summary>
+        /// 값을 읽어 온다. 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="section"></param>
+        /// <param name="key"></param>
+        /// <param name="def"></param>
+        /// <returns></returns>
+        public static string GetValue(string path, string section, string key, string def = "")
+        {
+            string buff = new string(' ', __BUFF_SIZE);
+            int i = GetPrivateProfileString(section, key, def, buff, __BUFF_SIZE, path);
+            List<string> stuff = new List<string>(buff.ToString().Split('\0'));
+
+            Debug.WriteLine("READ {0}\t{1}\t{2}\t{3}", path, section, key, stuff[0].ToString());
+
+            return stuff[0].ToString();
+        }
+
+        /// <summary>
+        /// 값을 쓴다
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="section"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public static string SetValue(string path, string section, string key, string value)
+        {
+            WritePrivateProfileString(section, key, value, path);
+
+            Debug.WriteLine("WRITE {0}\t{1}\t{2}\t{3}", path, section, key, value);
+            return value;
+        }
+
+        /// <summary>
+        /// 값을 리스트로 읽어 온다
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="section">인자가 없으면, 섹션 리스트를 가져온다</param>
+        /// <param name="key">인자가 없으면, 키 리스트를 가져온다</param>
+        /// <returns></returns>
+        public static List<string> GetValueAll(string path, string section = null, string key = null)
+        {
+            const string no_value_found = "no_value_found";
+
+            string buff = new string(' ', __BUFF_SIZE);
+
+            GetPrivateProfileString(section, key, no_value_found, buff, __BUFF_SIZE, path);
+
+            List<string> stuff = new List<string>(buff.ToString().Split('\0'));
+
+            //쓰레기값 제거
+            var count = 0;
+            var idx = stuff.Count - 1;
+            for (; idx > 0; idx--)
+                if (!stuff[idx].Trim().Equals(string.Empty))
+                    break;
+                else
+                    count++;
+
+            stuff.RemoveRange(idx + 1, count);
+
+            foreach(var it in stuff)
+                Debug.WriteLine("LIST {0}\t{1}\t{2}\t{3}", path, section, key, it);
+
+            return stuff;
+        }
+
+    }
+}
---
+++ IniHelper.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Ini
+{
+    #region +class CKey
+
+    public class CKey
+    {
+        string section = string.Empty;
+        string key = string.Empty;
+        string value = string.Empty;
+
+        public string Section { 
+            get { return this.section; }
+            set { this.section = value; }
+        }
+        public string Key { 
+            get { return this.key; }
+            set { this.key = value; }
+        }
+        public string Value { 
+            get { return this.value; }
+            set { this.value = value; }
+        }
+
+        public override string ToString()
+        {
+            return this.value;
+        }
+
+        public int ToInt()
+        {
+            if (this.value.Length == 0)
+                this.value = "0";
+            return int.Parse(this.value);
+        }
+
+        public CKey(string section, string key, string value)
+        {
+            this.section = section;
+            this.key = key;
+            this.value = value;
+        }
+
+        public void Clear()
+        {
+            value = string.Empty;
+        }
+
+    }
+
+    #endregion
+
+    #region +class CSection
+
+    public class CSection
+    {
+        string section = string.Empty;
+
+        public Dictionary<string, CKey> value = new Dictionary<string, CKey>();
+
+        public CSection(string section, Dictionary<string, CKey> value)
+        {
+            this.section = section;
+            this.value = value;
+        }
+
+        public void Clear()
+        {
+            value = new Dictionary<string,CKey>();
+        }
+
+        public CKey Key(string key)
+        {
+            if (value.ContainsKey(key.ToLower()))
+                return value[key.ToLower()];
+
+            else return new CKey(string.Empty, string.Empty, string.Empty);
+        }
+
+        public void Add(string key, CKey dic)
+        {
+            value.Add(key.ToLower(), dic);
+        }
+    }
+
+    #endregion
+
+    #region +class IniHelper
+
+    public class IniHelper
+    {
+        Dictionary<string, CSection> value = new Dictionary<string, CSection>();
+
+        string __IniPath = string.Empty;
+
+
+        public IniHelper(string iniPath)
+        {
+            Initialize(iniPath);
+        }
+
+        void Initialize(string iniPath)
+        {
+            __IniPath = iniPath;
+
+            //Read();
+        }
+
+        public CSection Section(string section)
+        {
+            return value[section.ToLower()];
+        }
+
+        public void Add(string key, CSection dic)
+        {
+            value.Add(key.ToLower(), dic);
+        }
+
+        public void Read()
+        {
+            Dictionary<string, CSection> listSection = new Dictionary<string, CSection>();
+
+            List<string> slist = IniAPI.GetValueAll(__IniPath);
+
+            foreach (var s in slist)
+            {
+                Dictionary<string, CKey> listKey = new Dictionary<string, CKey>();
+
+                List<string> klist = IniAPI.GetValueAll(__IniPath, s);
+
+                foreach (var k in klist)
+                {
+                    string v = IniAPI.GetValueAll(__IniPath, s, k)[0];
+
+                    CKey key = new CKey(s,k,v);
+
+                    listKey.Add(k.ToLower(), key);
+                }
+
+                CSection section = new CSection(s, listKey);
+
+                listSection.Add(s.ToLower(), section);
+            }
+
+            value = listSection;
+
+            return;
+        }
+
+        public void Write()
+        {
+            foreach (var s in value)
+            {
+                foreach (var k in s.Value.value)
+                {
+                    IniAPI.SetValue(__IniPath, k.Value.Section, k.Value.Key, k.Value.Value);
+                }
+            }
+            return;
+        }
+
+
+    }
+
+    #endregion
+}
